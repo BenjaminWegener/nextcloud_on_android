63ff499f51839e170c88c3748214070a66f5db1b
@BenjaminWegener <wegeneredv@gmail.com>
2022-07-23 14:10:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-sudo apt install -y wget unzip sqlite php php-{cli,xml,zip,curl,gd,cgi,mysql,mbstring} lighttpd
+sudo apt install -y wget unzip sqlite php php-{cli,xml,zip,curl,gd,cgi,mysql,mbstring,xmlrpc,recode,mcrypt,tidy,imagick} libterm-readline-gnu-perl  lighttpd 
+sudo lighttpd-enable-mod fastcgi 
+sudo lighttpd-enable-mod fastcgi-php
 cd /var/www/html/
 wget --no-check-certificate https://download.nextcloud.com/server/releases/latest-23.zip
 unzip latest-23.zip
 sudo chown -R www-data:www-data /var/www/
 sudo chmod -R 755 /var/www/
 sudo sed -i 's/= 80/= 8080/g' /etc/lighttpd/lighttpd.conf
-sudo service lighttpd restart
+sudo systemctl stop lighttpd.service
+sudo systemctl start lighttpd.service
+sudo systemctl enable lighttpd.service
