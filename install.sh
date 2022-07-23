c3bd0cd619c4ba119fb284662459cfcfd8d136f5
@BenjaminWegener <wegeneredv@gmail.com>
2022-07-24 01:24:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,22 @@ pkg install -y unzip sqlite php
 wget --no-check-certificate https://download.nextcloud.com/server/releases/latest-24.zip
 unzip latest-24.zip
 sed -i 's/localhost:8080/*/g' ./nextcloud/config/config.sample.php
-php -S localhost:8080 -t ./nextcloud
+#php -S localhost:8080 -t ./nextcloud
+echo '
+server.port             = 8080
+server.document-root    = "~/nextcloud"
+server.upload-dirs      = ( "/data/data/com.termux/files/usr/tmp" )
+index-file.names        = ( "index.html" )
+mimetype.assign         = (
+                                ".html" => "text/html",
+                                ".txt" => "text/plain",
+                                ".css" => "text/css",
+                                ".js" => "application/x-javascript",
+                                ".jpg" => "image/jpeg",
+                                ".jpeg" => "image/jpeg",
+                                ".gif" => "image/gif",
+                                ".png" => "image/png",
+                                "" => "application/octet-stream"
+                        )
+' > lighttpd.conf
+lighttpd -f lighttpd.conf
