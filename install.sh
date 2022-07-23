f8eeecf5c7e01a9d9f13d9eecd1b7329a5dbd07d
@BenjaminWegener <wegeneredv@gmail.com>
2022-07-23 10:59:06 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install -y wget unzip sqlite php php-{cli,xml,zip,curl,gd,cgi,mysql,mbstring} lighttpd
+cd /var/www/html/
+wget --no-check-certificate https://download.nextcloud.com/server/releases/latest-23.zip
+unzip latest-23.zip
+sudo chown -R www-data:www-data /var/www/html/nextcloud
+sudo sudo chmod -R 755 /var/www/html/nextcloud
+sudo sed -i 's/80/8080/g' /etc/lighttpd/lighttpd.conf
+sudo service lighttpd restart
