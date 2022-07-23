e8d2bab6ac92bb1ee2e8b4a75a87cfd3db1c8f61
@BenjaminWegener <wegeneredv@gmail.com>
2022-07-23 12:22:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ wget --no-check-certificate https://download.nextcloud.com/server/releases/lates
 unzip latest-23.zip
 sudo chown -R www-data:www-data /var/www/
 sudo chmod -R 755 /var/www/
-sudo sed -i 's/80/8080/g' /etc/lighttpd/lighttpd.conf
+sudo sed -i 's/= 80/= 8080/g' /etc/lighttpd/lighttpd.conf
 sudo service lighttpd restart
