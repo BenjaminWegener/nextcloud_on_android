770589313a9fd354f37dad84b7f6c6dd1210bc93
@BenjaminWegener <wegeneredv@gmail.com>
2022-07-24 00:45:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ pkg install -y unzip sqlite php
 wget --no-check-certificate https://download.nextcloud.com/server/releases/latest-24.zip
 unzip latest-24.zip
 sed -i 's/localhost:8080/*/g' ./nextcloud/config/config.sample.php
+php -S localhost:8080 -t ./nextcloud
